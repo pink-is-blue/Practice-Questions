44f4838e184dcd587dae99a3c414066059cc4c09
pink-is-blue <137195648+pink-is-blue@users.noreply.github.com>
2025-01-29 20:16:21 +0530

    Add files via upload

---
+++ exercise28_1-2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+#using absolute fuction to make anything positive
+addition = abs(num1 + num2)
+subtraction = abs(num1 - num2)
+
+print("Addition of 2 values: ", addition)
+print("Subtraction of 2 values: ", subtraction)
