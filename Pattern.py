44d110c667dfe24ed509d7e0818d7a7da62a9025
pink-is-blue <137195648+pink-is-blue@users.noreply.github.com>
2025-02-02 22:41:04 +0530

    Update

---
+++ Pattern.py
@@ -0,0 +1,7 @@
+n = 7  
+
+for i in range(1, n + 1, 2):  #first half
+    print("*" * i)#prints till the value of n
+
+for i in range(n - 2, 0, -2):  #2nd half
+    print("*" * i)#reduces to zero from n
